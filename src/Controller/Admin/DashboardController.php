0095606bfbdf5faf973b95575830b75109468d4a
EtienneGuignard <etienneguignard2@gmail.com>
2022-06-30 15:13:54 +0200

    dashboard v1

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller\Admin;
 
+use App\Controller\CampaignController;
+use App\Controller\ChartController;
+use App\Entity\Campaign;
+use App\Entity\Leads;
 use App\Entity\RuleGroup;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
@@ -13,42 +18,33 @@ use Symfony\UX\Chartjs\Model\Chart;
 
 class DashboardController extends AbstractDashboardController
 {
+  
+    public function __construct(
+        private AdminUrlGenerator $adminUrlGenerator,
+        private ChartBuilderInterface $chartBuilder
+    )
+    {
+    
+    }
     #[Route('/admin', name: 'admin')]
-    public function testdashbord(ChartBuilderInterface $chartBuilder): Response
+    public function index(): Response
     {
 
-        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
-        $chart->setData([
-            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            'datasets' => [
-                [
-                    'label' => 'My First dataset',
-                    'backgroundColor' => 'rgb(255, 99, 132)',
-                    'borderColor' => 'rgb(255, 99, 132)',
-                    'data' => [0, 10, 5, 2, 20, 30, 45],
-                ],
-            ],
-        ]);
-
-        $chart->setOptions([
-            'scales' => [
-                'y' => [
-                    'suggestedMin' => 0,
-                    'suggestedMax' => 100,
-                ],
-            ],
-        ]);
+        $url= $this->adminUrlGenerator
+                ->setController(CampaignCrudController::class)
+                ->generateUrl();
+                
 
 
-        
-        return $this->render('admin/dashboard.html.twig', [
-            'chart' => $chart,
-        ]);
+        return $this->redirect($url);
+        // return $this->render('admin/index.html.twig', [
+        //     'chart' => $this->chart(),
+        // ]);
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
         // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+        
 
         // Option 2. You can make your dashboard redirect to different pages depending on the user
         //
@@ -63,6 +59,33 @@ class DashboardController extends AbstractDashboardController
         // return $this->render('some/path/my-dashboard.html.twig');
     }
 
+
+    private function chart(): Chart
+    {
+        
+        $chart = $this->chartBuilder->createChart(Chart::TYPE_LINE);
+        $chart->setData([
+            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'datasets' => [
+                [
+                    'label' => 'My First dataset',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [0, 10, 5, 2, 20, 30, 45],
+                ],
+            ],
+        ]);
+        $chart->setOptions([
+            'scales' => [
+                'y' => [
+                'suggestedMin' => 0,
+                'suggestedMax' => 100,
+                ],
+            ],
+        ]);
+        return $chart;
+    }
+
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
@@ -72,6 +95,8 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('The Label', 'fas fa-list', RuleGroup::class);
+        yield MenuItem::linkToCrud('Rule group', 'fas fa-list', RuleGroup::class);
+        yield MenuItem::linkToCrud('Campaign', 'fas fa-bullhorn', Campaign::class);
+        yield MenuItem::linkToCrud('Leads', 'fas fa-account', Leads::class);
     }
 }
--- src/Controller/Admin/RuleGroupCrudController.php
+++ src/Controller/Admin/RuleGroupCrudController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\RuleGroup;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class RuleGroupCrudController extends AbstractCrudController
 {
@@ -12,14 +14,14 @@ class RuleGroupCrudController extends AbstractCrudController
         return RuleGroup::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            // IdField::new('id'),
+            TextField::new('name'),
+            // TextEditorField::new('description'),
         ];
     }
-    */
+
 }

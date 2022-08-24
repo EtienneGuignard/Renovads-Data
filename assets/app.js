3052cd5437a23825e2232390814e8246cd885c67
EtienneGuignard <93521714+EtienneGuignard@users.noreply.github.com>
2022-08-24 14:35:27 +0200

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -17,5 +17,5 @@ import './bootstrap';
 const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
-require('bootstrap');
+// require('bootstrap');
 

502bc445368170097f059b30a6efb3087ae36f0f
EtienneGuignard <93521714+EtienneGuignard@users.noreply.github.com>
2022-08-24 13:04:44 +0200

    hero-v2

--- assets/app.js
+++ assets/app.js
@@ -10,7 +10,7 @@
 import './styles/app.scss';
 
 // start the Stimulus application
-import './bootstrap';
+// import './bootstrap';
 
 // activates collapse functionality
 import { Collapse } from 'bootstrap';

077111fac729d3c4a94721f1a65cfee85fcfb80a
EtienneGuignard <93521714+EtienneGuignard@users.noreply.github.com>
2022-08-26 15:54:32 +0200

    show password plus bug fix

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -21,7 +22,7 @@ class RegistrationFormType extends AbstractType
         $builder
             ->add('email', EmailType::class, [
                 'label' => false,
-                'attr'=>['class' => 'form-control']]
+                'attr'=>['class' => 'form-control mb-2']]
                 )
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
@@ -32,12 +33,18 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
+            ->add('plainPassword', RepeatedType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'type'=>ShowHidePasswordType::class,
+                'invalid_message' => 'The passwords are not identical.',
                 'mapped' => false,
                 'label' => false,
                 'attr' => ['autocomplete' => 'new-password'],
+                'options' =>        ['attr' => ['class' => 'form-control password-field']],
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
+
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',

84177974d3d3ebc2ebf800cfdd6ff54427f0973a
GroupeAO <etienneguignard2@gmail.com>
2022-09-04 18:33:14 +0200

    modif crud

--- src/Controller/Admin/ApiTokenCrudController.php
+++ src/Controller/Admin/ApiTokenCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\ApiToken;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
@@ -14,6 +17,11 @@ class ApiTokenCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return ApiToken::class;
+    }
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+        ->add(Crud::PAGE_INDEX, Action::DETAIL);
     }
 
     

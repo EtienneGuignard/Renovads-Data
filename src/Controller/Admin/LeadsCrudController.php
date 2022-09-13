1fe16dc1f81e001cad6f1674f0a3777794f0a655
GroupeAO <etienneguignard2@gmail.com>
2022-09-13 13:18:31 +0200

    Update LeadsCrudController.php

--- src/Controller/Admin/LeadsCrudController.php
+++ src/Controller/Admin/LeadsCrudController.php
@@ -13,6 +13,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class LeadsCrudController extends AbstractCrudController

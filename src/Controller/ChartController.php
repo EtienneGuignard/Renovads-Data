afb214a5b6b3afd48ca519da712371ce3d226118
EtienneGuignard <93521714+EtienneGuignard@users.noreply.github.com>
2022-10-18 14:58:12 +0200

    Update ChartController.php

--- src/Controller/ChartController.php
+++ src/Controller/ChartController.php
@@ -35,7 +35,6 @@ class ChartController extends AbstractController
                 ],
             ],
         ]);
-
         $chart->setOptions([
             'scales' => [
                 'y' => [
